6f355b8dc122088520460b223b45072c4db9bbcf
Randy Hoyt <randy@randyhoyt.com>
2011-11-03 22:39:39 -0500

    Matching trunk

--- example-functions.php
+++ example-functions.php
@@ -160,7 +160,7 @@ function be_sample_metaboxes( $meta_boxes ) {
 
 
 // Initialize the metabox class
-add_action( 'init', 'be_initialize_cmb_meta_boxes' ,9999 );
+add_action('init','be_initialize_cmb_meta_boxes',9999 );
 function be_initialize_cmb_meta_boxes() {
     if ( !class_exists( 'cmb_Meta_Box' ) ) {
         require_once( 'init.php' );
