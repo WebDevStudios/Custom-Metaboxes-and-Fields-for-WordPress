94ff23c7409571360d6fa4cebcdd466ca37ecdda
Tom Willmot <tom@humanmade.co.uk>
2012-02-17 18:47:36 +0000

    Initial bash at text_cloneable.
    Conflicts:
    
    	additions.cloneable_text.php

--- additions.cloneable_text.php
+++ additions.cloneable_text.php
@@ -1,69 +1,91 @@
 <?php
 
-add_action( 'cmb_render_cloneable_text', function( $field, $meta ) {
+/**
+ * Render a cloneable text field
+ *
+ * @param string $field
+ * @return null
+ */
+function cmb_render_text_cloneable( $field, $meta ) { ?>
 
-	// meta won't have the multiples so get it again
-	global $post;
-	
-	$meta = get_post_meta( $post->ID, $field['id'], false );
-	
-	?>
-	
 	<div class="cmb-cloneable-text-inputs">
-		<?php foreach ( $meta as $meta_value ) : ?>
-			<input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]" value="<?php echo $meta_value ?>"><a href="#" class="button cmb-cloneable-text-remove">X</a>
-		<?php endforeach; ?>
-		
-		<?php if ( ! $meta ) : ?>
-		
-			<input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]" value=""><a href="#" class="button cmb-cloneable-text-remove">X</a>
-		
-		<?php endif; ?>
+
+	<?php foreach ( $meta as $key => $meta_value ) : ?>
+
+	    <input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]" value="<?php echo $meta_value ?>">
+
+	<?php endforeach; ?>
+
+	<?php if ( ! $meta ) : ?>
+
+	    <input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]">
+
+	<?php endif; ?>
+
 	</div>
-	
-	<span class="cmb-cloneable-text-template" style="display:none;">
-		<input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]" value=""><a href="#" class="button cmb-cloneable-text-remove">X</a>
-	</span>
-	
+
 	<p>
-		<a class="button cmb-cloneable-text-add-new" href="#">Add New</a>
+		<button class="button cmb-cloneable-text-add-new">Add Field</button>
 	</p>
-	
+
 	<?php
-	
-	// only add the javascript once
+
+	// Only add the javascript once
 	static $added_js;
-	
-	if ( ! $added_js ) : $added_js = true;?>
-		
+
+	if ( ! $added_js ) :
+
+		$added_js = true; ?>
+
 		<script type="text/javascript">
 			jQuery( document ).ready( function() {
-			
-				jQuery( '.cmb-cloneable-text-add-new' ).live( 'click', function(e) {
+
+				closeButton = '<button class="button cmb-cloneable-text-remove">Remove</button>';
+				field = '<input style="width: 92%" type="text" name="<?php echo $field['id'] ?>[]" value="">';
+
+				jQuery( '.cmb-cloneable-text-inputs input:not(:first)' ).each( function() {
+
+					console.log( this );
+
+					jQuery( this ).after( closeButton );
+
+				} );
+
+				jQuery( '.cmb-cloneable-text-add-new' ).live( 'click', function( e ) {
+
 					e.preventDefault();
-					
-					var newT = jQuery( '.cmb-cloneable-text-template' ).children().clone();
-					jQuery( '.cmb-cloneable-text-inputs' ).append( newT );
-					
+
+					jQuery( '.cmb-cloneable-text-inputs' ).append( field ).append( closeButton );
+
 				} )
-				
-				jQuery( '.cmb-cloneable-text-remove' ).live( 'click', function(e) {
+
+				jQuery( '.cmb-cloneable-text-remove' ).live( 'click', function( e ) {
+
 					e.preventDefault();
-					
+
 					jQuery( this ).prev().remove();
 					jQuery( this ).remove();
-					
+
 				} )
-			
+
 			} );
 		</script>
+
 	<?php endif;
 
-}, 10, 2 );
+}
+add_action( 'cmb_render_text_cloneable', 'cmb_render_text_cloneable', 10, 2 );
 
+/**
+ * Validate the cloneable text field data
+ *
+ * @param array $data
+ * @return array
+ */
+function cmb_validate_text_cloneable( array $data ) {
 
-add_filter( 'cmb_validate_cloneable_text', function( $data ) {
-	$data = (array) $data;
 	sort( $data );
 	return array_filter( $data );
-} );
+
+}
+add_filter( 'cmb_validate_text_cloneable', 'cmb_validate_text_cloneable' );
