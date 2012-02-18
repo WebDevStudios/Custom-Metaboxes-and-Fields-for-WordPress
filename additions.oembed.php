b594f3d9fb2ef364360ef0053f7127809c6eaede
Tom Willmot <tom@humanmade.co.uk>
2012-02-18 18:30:13 +0000

    Update oembed to handle multiple fields

--- additions.oembed.php
+++ additions.oembed.php
@@ -20,10 +20,17 @@ add_action( 'cmb_render_oembed', 'cmb_render_oembed', 10, 2 );
  */
 function cmb_validate_oembed( $new, $post_id, $field ) {
 
-	$oembed = wp_oembed_get( esc_url( $new ) );
+	delete_post_meta( $post_id, $field['id'] . '_code' );
+
+	// Run each url through wp_oembed_get and save the embed code
+	foreach( (array) $new as $url ) {
+
+		$oembed = wp_oembed_get( $url, array( 'height' => 75 ) );
 	
-	if ( $oembed )
-		update_post_meta( $post_id, $field['id'] . '_code', $oembed );
+		if ( $oembed )
+			add_post_meta( $post_id, $field['id'] . '_code', $oembed );
+			
+	}
 		
 	return $new;
 
