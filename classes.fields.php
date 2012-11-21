7a6715e1adc875dbf06fb7dc6617bdf9072d14d1
Joe Hoyle <joehoyle@gmail.com>
2012-11-21 13:49:15 +0000

    don't update boxes on other pages

--- classes.fields.php
+++ classes.fields.php
@@ -273,8 +273,8 @@ class CMB_File_Field extends CMB_Field {
 	public function html() {
 
 		?>
-		<a class="button cmb-file-upload" class="<?php echo $this->get_value() ? 'hidden' : '' ?>" href="#">Upload file</a>
-		<div class="<?php echo $this->get_value() ? '' : 'hidden' ?>" style="width: 200px; height: 120px; padding: 5px; border-radius: 5px; background: #eee; text-align: center;">
+		<a class="button cmb-file-upload <?php echo $this->get_value() ? 'hidden' : '' ?>" href="#">Upload file</a>
+		<div class="<?php echo $this->get_value() ? '' : 'hidden' ?>" style="width: 200px; padding: 5px; border-radius: 5px; background: #eee; text-align: center;">
 			<div class="cmb-file-holder" style="text-align: center; vertical-align: middle;">
 				<?php if ( $this->get_value() ) : ?>
 					<?php echo wp_get_attachment_image( $this->get_value(),'thumbnail', true ) ?>
