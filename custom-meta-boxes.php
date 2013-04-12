1b95a6a3ff7684e525dd159c49d3ebc88a1ae4dc
Matth_eu <matthew@matth.eu>
2013-04-12 10:08:37 +0100

    Split getting the class mapping array, and getting class for type into separate functions

--- custom-meta-boxes.php
+++ custom-meta-boxes.php
@@ -79,32 +79,41 @@ function cmb_scripts( $hook ) {
 }
 add_action( 'admin_enqueue_scripts', 'cmb_scripts', 10 );
 
-function _cmb_field_class_for_type( $type ) {
+/**
+ * Return an array of built in available fields
+ *
+ * Key is field name, Value is class used by field.
+ * Available fields can be modified using the 'cmb_field_types' filter.
+ * 
+ * @return array
+ */
+function _cmb_available_fields() {
 
-	$map = apply_filters( 'cmb_field_types', array(
-
+	return apply_filters( 'cmb_field_types', array(
 		'text'				=> 'CMB_Text_Field',
 		'text_small' 		=> 'CMB_Text_Small_Field',
 		'text_url'			=> 'CMB_URL_Field',
 		'url'				=> 'CMB_URL_Field',
+		'radio'				=> 'CMB_Radio_Field',
+		'checkbox'			=> 'CMB_Checkbox',
 		'file'				=> 'CMB_File_Field',
 		'image' 			=> 'CMB_Image_Field',
-		'group'				=> 'CMB_Group_Field',
 		'oembed'			=> 'CMB_Oembed_Field',
+		'wysiwyg'			=> 'CMB_wysiwyg',
+		'textarea'			=> 'CMB_Textarea_Field',
+		'textarea_code'		=> 'CMB_Textarea_Field_Code',
+		'select'			=> 'CMB_Select',
+		'taxonomy_select'	=> 'CMB_Taxonomy',
+		'post_select'		=> 'CMB_Post_Select',
 		'date'				=> 'CMB_Date_Field',
 		'date_unix'			=> 'CMB_Date_Timestamp_Field',
 		'datetime_unix'		=> 'CMB_Datetime_Timestamp_Field',
 		'time'				=> 'CMB_Time_Field',
-		'textarea'			=> 'CMB_Textarea_Field',
-		'textarea_code'		=> 'CMB_Textarea_Field_Code',
-		'taxonomy_select'	=> 'CMB_Taxonomy',
-		'select'			=> 'CMB_Select',
-		'wysiwyg'			=> 'CMB_wysiwyg',
-		'checkbox'			=> 'CMB_Checkbox',
-		'post_select'		=> 'CMB_Post_Select',
-		'radio'				=> 'CMB_Radio_Field'
 		'title'				=> 'CMB_Title',
+		'group'				=> 'CMB_Group_Field',
 	) );
+
+}
 
 /**
  * Get a field class by type
@@ -112,10 +121,14 @@ function _cmb_field_class_for_type( $type ) {
  * @param  string $type 
  * @return string $class, or false if not found.
  */
+function _cmb_field_class_for_type( $type ) {
+
+	$map = _cmb_available_fields();
+
 	if ( isset( $map[$type] ) )
 		return $map[$type];
 
-	return $type;
+	return false;
 
 }
 
