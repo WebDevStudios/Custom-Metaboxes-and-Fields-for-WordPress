1b311a3763405717e7583902db73ac745d6341e8
Jurgens Banninga <tooone777@gmail.com>
2013-04-18 10:51:47 +0200

    Initialze select2 on selects with the 'cmb_select2' class

--- js/cmb.js
+++ js/cmb.js
@@ -33,6 +33,11 @@ jQuery(document).ready(function ($) {
 	});
 
 	/**
+	 * Initialize select2 (advanced select)
+	 */
+	 $('.cmb_select2').select2({});
+
+	/**
 	 * Initialize jQuery UI datepicker (this will be moved inline in a future release)
 	 */
 	$('.cmb_datepicker').each(function () {
