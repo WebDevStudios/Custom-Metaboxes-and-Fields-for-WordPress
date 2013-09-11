24edbb17e820dc063f749909c58299d0e6b32268
Matth_eu <matthew@matth.eu>
2013-09-11 18:50:56 +0100

    simpler file upload script. + use ajax callback to fetch correct image + support different types

--- js/file-upload.js
+++ js/file-upload.js
@@ -4,102 +4,78 @@ jQuery( document ).ready( function() {
 
 		e.preventDefault();
 
-		var container = jQuery( this ).parent();
 		var link = jQuery( this );
+		var container = jQuery( this ).parent();
 
-		var frame = wp.media( {
-			state: 'cmb-file',
-			states: [ new CMBSelectFileController() ]
-		})
+		var frameArgs = {
+			multiple: false,
+			title: 'Select File',
+		}
 
-		frame.on( 'toolbar:create:cmb-file', function( toolbar ) {
-			this.createSelectToolbar( toolbar, {
-				text: 'Select file'
-			});
-		}, frame );
+		library = container.attr( 'data-type' ).split(',');
+		if ( library.length > 0 )
+			frameArgs.library = { type: library }
 
-		frame.open();
+		var CMB_Frame = wp.media( frameArgs );
 
-		frame.state('cmb-file').on( 'select', function() {
+		CMB_Frame.on( 'select', function() {
 
-			var selection = frame.state().get('selection'),
+			var selection = CMB_Frame.state().get('selection'),
 				model = selection.first(),
 				fileHolder = container.find( '.cmb-file-holder' );
 
 			jQuery( container ).find( '.cmb-file-upload-input' ).val( model.id );
-			link.hide();
 
-			frame.close();
+			link.hide(); // Hide 'add media' button
+
+			CMB_Frame.close();
 
 			var fileClass = ( model.attributes.type === 'image' ) ? 'type-image' : 'type-file';
 			fileHolder.addClass( fileClass );
 			fileHolder.html( '' );
 			fileHolder.parent().show();
 
-			jQuery( '<img />', {
-				src: model.attributes.type === 'image' ? model.attributes.sizes.thumbnail.url : model.attributes.icon
-			}).prependTo( container.find( '.cmb-file-holder' ) );
+			if ( model.attributes.type === 'image' ) {
 
-			if ( model.attributes.type !== 'image' )
+				var data = {
+					action: 'cmb_request_image',
+					id: model.attributes.id,
+					width: container.width(),
+					height: container.height(),
+					crop: fileHolder.attr('data-crop')
+				}
+
+				jQuery.post( ajaxurl, data, function( src ) {
+					// Insert image
+					jQuery( '<img />', { src: src } ).prependTo( fileHolder );
+				}).fail( function() {
+					// Fallback - insert full size image.
+					jQuery( '<img />', { src: model.attributes.url } ).prependTo( fileHolder );
+				});
+
+			} else {
+
+				jQuery( '<img />', { src: model.attributes.icon } ).prependTo( fileHolder );
 				fileHolder.append( jQuery('<div class="cmb-file-name" />').html( '<strong>' + model.attributes.filename + '</strong>' ) );
 
+			}
+
 		});
+
+		CMB_Frame.open();
 
 	} );
 
 	jQuery( document ).on( 'click', '.cmb-remove-file', function(e) {
+
 		e.preventDefault();
+
 		var container = jQuery( this ).parent().parent();
+
 		container.find( '.cmb-file-holder' ).html( '' ).parent().hide();
 		container.find( '.cmb-file-upload-input' ).val( '' );
-		container.find( '.cmb-file-upload' ).show();
+		container.find( '.cmb-file-upload' ).show().css( 'display', 'inline-block' );
+
 	} );
-
-	// wp.media.controller.FeaturedImage
-	// ---------------------------------
-	var CMBSelectFileController = wp.media.controller.Library.extend({
-		defaults: _.defaults({
-			id:         'cmb-file',
-			filterable: 'uploaded',
-			multiple:   false,
-			toolbar:    'cmb-file',
-			title:      'Select File',
-			priority:   60,
-			syncSelection: false
-		}, wp.media.controller.Library.prototype.defaults ),
-
-		initialize: function() {
-			var library, comparator;
-
-			wp.media.controller.Library.prototype.initialize.apply( this, arguments );
-
-			library    = this.get('library');
-			comparator = library.comparator;
-		},
-
-		activate: function() {
-			this.updateSelection();
-			this.frame.on( 'open', this.updateSelection, this );
-			wp.media.controller.Library.prototype.activate.apply( this, arguments );
-		},
-
-		deactivate: function() {
-			this.frame.off( 'open', this.updateSelection, this );
-			wp.media.controller.Library.prototype.deactivate.apply( this, arguments );
-		},
-
-		updateSelection: function() {
-			var selection = this.get('selection'),
-				id = wp.media.view.settings.post.featuredImageId,
-				attachment;
-
-			if ( '' !== id && -1 !== id ) {
-				attachment = wp.media.model.Attachment.get( id );
-				attachment.fetch();
-			}
-
-			selection.reset( attachment ? [ attachment ] : [] );
-		}
-	});
 
 } );
