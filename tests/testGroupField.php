f5c63ea4ab9a05bd4d7b228c3d5484314fa87894
Matth_eu <matthew@matth.eu>
2013-11-25 15:52:33 +0000

    Unit tests

--- tests/testGroupField.php
+++ tests/testGroupField.php
@@ -4,25 +4,33 @@ class GroupFieldTestCase extends WP_UnitTestCase {
 
 	function testAddField() {
 
-		$group = new CMB_Group_Field( 'group', 'Group Title', array() );
-		$field = new CMB_Text_Field( 'foo', 'Title', array( 1, 2 ) );
+		$group  = new CMB_Group_Field( 'group', 'Group Title', array() );
+		$field1 = new CMB_Text_Field( 'foo', 'Title', array( 1, 2 ) );
+		$field2 = new CMB_Text_Field( 'bar', 'Title', array( 3, 4 ) );
 
-		$group->add_field( $field );
+		$group->add_field( $field1 );
+		$group->add_field( $field2 );
 	
-		$this->assertEquals( $field->name, 'group[foo][][]' );
+		$this->assertArrayHasKey( 'foo', $group->get_fields() );
+		$this->assertArrayHasKey( 'bar', $group->get_fields() );
 	
 	}
 
-	function testSetValues() {
+	function testGetValues() {
 		
-		$group = new CMB_Group_Field( 'group', 'Group Title', array() );
-		$field = new CMB_Text_Field( 'foo', 'Title', array() );
-
-		$group->add_field( $field );
+		$group  = new CMB_Group_Field( 'group', 'Group Title', array() );
+		$field1 = new CMB_Text_Field( 'foo', 'Title', array() );
+		$field2 = new CMB_Text_Field( 'bar', 'Title', array() );
+	
+		$group->add_field( $field1 );
+		$group->add_field( $field2 );
 		
-		$group->set_values( $values = array( 
-			array( 'foo' => array( 1, 2 ) ) 
-		) );
+		$group->values = $values = array( 
+			'group' => array( 
+				'foo' => array( 1, 2 ),
+				'bar' => array( 3, 4 ) 
+			)
+		);
 
 		$this->assertEquals( $group->get_values(), $values );
 	
@@ -30,19 +38,82 @@ class GroupFieldTestCase extends WP_UnitTestCase {
 
 	function testParseSaveValues() {
 
-		$group = new CMB_Group_Field( 'group', 'Group Title', array() );
-		$field = new CMB_Text_Field( 'foo', 'Title', array() );
-
-		$group->add_field( $field );
-		$group->values = $values = array( 
-				'foo' => array( 1, 2 ) 
-		);
+		$group  = new CMB_Group_Field( 'group', 'Group Title', array() );
+		$field1 = new CMB_Text_Field( 'foo', 'Title', array( 1, 2 ) );
+		$field2 = new CMB_Text_Field( 'bar', 'Title', array( 3, 4 ) );
+	
+		$group->add_field( $field1 );
+		$group->add_field( $field2 );
 		
+		$group->set_values( $values = array( 
+			'group' => array( 
+				'foo' => array( 1, 2 ) ,
+				'bar' => array( 1, 2 ) 
+			),
+		) );
+
 		$group->parse_save_values();
 
-		error_log( print_r( $group->get_values(), true ) );
-		
 		$this->assertEquals( $group->get_values(), $values );
 
 	}
+
+	function testFieldNameAttrValue() {
+
+		$group  = new CMB_Group_Field( 'group', 'Group Title', array() );
+		$field1 = new CMB_Text_Field( 'foo', 'Title', array( 1, 2 ) );
+	
+		$group->add_field( $field1 );
+
+		// Standard use of ID attribute
+		$id_attr = $field1->get_the_name_attr();
+		$this->assertEquals( $id_attr, 'group[cmb-group-0][foo][cmb-field-0]' );
+
+		// Using append
+		$id_attr = $field1->get_the_name_attr( '[bar]' );
+		$this->assertEquals( $id_attr, 'group[cmb-group-0][foo][cmb-field-0][bar]' );
+
+		// Test repeatable group.
+		$group->field_index = 1;
+		$id_attr = $field1->get_the_name_attr();
+		$this->assertEquals( $id_attr, 'group[cmb-group-1][foo][cmb-field-0]' );
+		$group->field_index = 0; // Unset
+
+		// Test repeatable field within group.
+		$field1->field_index = 1;
+		$id_attr = $field1->get_the_name_attr();
+		$this->assertEquals( $id_attr, 'group[cmb-group-0][foo][cmb-field-1]' );
+		$field1->field_index = 0; // Unset
+
+	}
+
+	function testFieldIdAttrValue() {
+
+		$group  = new CMB_Group_Field( 'group', 'Group Title', array() );
+		$field1 = new CMB_Text_Field( 'foo', 'Title', array( 1, 2 ) );
+	
+		$group->add_field( $field1 );
+
+		// Standard use of ID attribute
+		$id_attr = $field1->get_the_id_attr();
+		$this->assertEquals( $id_attr, 'group-cmb-group-0-foo-cmb-field-0' );
+
+		// Using append
+		$id_attr = $field1->get_the_id_attr( 'bar' );
+		$this->assertEquals( $id_attr, 'group-cmb-group-0-foo-cmb-field-0-bar' );
+
+		// Test repeatable group.
+		$group->field_index = 1;
+		$id_attr = $field1->get_the_id_attr();
+		$this->assertEquals( $id_attr, 'group-cmb-group-1-foo-cmb-field-0' );
+		$group->field_index = 0; // Unset
+
+		// Test repeatable field within group.
+		$field1->field_index = 1;
+		$id_attr = $field1->get_the_id_attr();
+		$this->assertEquals( $id_attr, 'group-cmb-group-0-foo-cmb-field-1' );
+		$field1->field_index = 0; // Unset
+
+	}
+
 }
